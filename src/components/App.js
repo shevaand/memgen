ecb9a496055bfa452e6505c1768ef57d3394df17
shevaand <bartsimpsonpvp@Gmail.com>
2019-10-05 10:48:22 +0300

    React APP

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "./Header";
+import MemeGenerator from "./MemeGenerator";
+
+import '../styles/App.css';
+
+function App() {
+        return (
+            <div>
+                <Header />
+                <MemeGenerator />
+            </div>
+        );
+}
+
+export default App;
